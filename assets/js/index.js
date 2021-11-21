d3c9b6ffcac800daf0b5b7683d294a3d60479f51
Alexis Vacilli <cyusa03@gmail.com>
2021-11-21 14:46:34 +0200

    product cards

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelector(".tabs");
+const card = document.querySelector(".card");
+const cardContent = document.querySelector(".card-content");
+
+const menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("mouseover", () => {
+  tabs.style.display = "block";
+  cardContent.style.margin = "21px 15px";
+});
+
+tabs.addEventListener("mouseleave", () => {
+  tabs.style.display = "none";
+  cardContent.style.margin = "10px 15px";
+});
+
+window.addEventListener("click", (e) => {
+      if(e.target !== tabs) {
+          tabs.style.display = "none";
+          cardContent.style.margin = "10px 15px"; 
+      }
+});
