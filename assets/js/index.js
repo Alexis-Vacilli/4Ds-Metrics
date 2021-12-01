d5e4daaf2df62e3f187e5d3d66bd6b4c78ad92a8
Alexis Vacilli <cyusa03@gmail.com>
2021-12-01 09:23:21 +0200

    automating the card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,42 @@
-const tabs = document.querySelector(".tabs");
-const card = document.querySelector(".card");
-const cardContent = document.querySelector(".card-content");
+const cards = document.querySelectorAll(".card");
+const tabs = document.querySelectorAll(".tabs");
+const cardContent = document.querySelectorAll(".card-content");
+const menuIcon = document.querySelectorAll(".menu-icon");
+
+
+const cardsArr = Array.from(cards);
+cardsArr.forEach(el => console.log(el.target));
 
 
 
-const menuIcon = document.querySelector(".menu-icon");
-menuIcon.addEventListener("mouseover", () => {
-  tabs.style.display = "block";
-  cardContent.style.margin = "21px 15px";
-});
 
-tabs.addEventListener("mouseleave", () => {
-  tabs.style.display = "none";
-  cardContent.style.margin = "10px 15px";
-});
 
-window.addEventListener("click", (e) => {
-      if(e.target !== tabs) {
-          tabs.style.display = "none";
-          cardContent.style.margin = "10px 15px"; 
-      }
-});
+Array.from(menuIcon).forEach(el => {el.addEventListener("mouseover", (e) => {
+    const element = document.elementFromPoint(el.after, el.before);
+    console.log(element)
+
+    
+    // Array.from(tabs).forEach(element => element.style.display = "block");
+    // Array.from(cardContent).forEach(element => element.style.margin = "21px 15px");
+})})
+
+// const menuIcon = document.querySelectorAll(".menu-icon");
+// menuIcon.addEventListener("mouseover", () => {
+//   tabs.style.display = "block";
+//   cardContent.style.margin = "21px 15px";
+// });
+
+
+
+
+// tabs.addEventListener("mouseleave", () => {
+//   tabs.style.display = "none";
+//   cardContent.style.margin = "10px 15px";
+// });
+
+// window.addEventListener("click", (e) => {
+//       if(e.target !== tabs) {
+//           tabs.style.display = "none";
+//           cardContent.style.margin = "10px 15px"; 
+//       }
+// });
